b1eadabbc410956ac7998640ab4e38c73858e3f0
Joseph <user.example.com>
2019-03-02 14:45:00 +0000

    Setup DI - Add NetworkModule

---
+++ app/src/main/java/com/challenge/myweatherapp/di/modules/NetworkModule.java
@@ -0,0 +1,32 @@
+package com.challenge.myweatherapp.di.modules;
+
+import com.challenge.myweatherapp.service.WeatherService;
+import com.challenge.myweatherapp.util.Constants;
+
+import javax.inject.Singleton;
+
+import dagger.Module;
+import dagger.Provides;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+@Module
+public class NetworkModule {
+
+    @Singleton
+    @Provides
+    Retrofit provideRetrofit() {
+        return new Retrofit.Builder().baseUrl(Constants.BASE_URL)
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+    }
+
+    @Singleton
+    @Provides
+    WeatherService provideRetrofitService(Retrofit retrofit) {
+        return retrofit.create(WeatherService.class);
+    }
+
+}
