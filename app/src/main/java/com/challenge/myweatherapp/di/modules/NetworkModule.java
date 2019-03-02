78bbcc507461b8047856add14104633a407c9c82
Joseph <user.example.com>
2019-03-02 17:17:19 +0000

    Add OkHttpInterceptor to NetworkModule

--- app/src/main/java/com/challenge/myweatherapp/di/modules/NetworkModule.java
+++ app/src/main/java/com/challenge/myweatherapp/di/modules/NetworkModule.java
@@ -3,16 +3,58 @@ package com.challenge.myweatherapp.di.modules;
 import com.challenge.myweatherapp.service.WeatherService;
 import com.challenge.myweatherapp.util.Constants;
 
+import java.util.concurrent.TimeUnit;
+
 import javax.inject.Singleton;
 
 import dagger.Module;
 import dagger.Provides;
+import okhttp3.HttpUrl;
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 @Module
 public class NetworkModule {
+
+    @Provides
+    @Singleton
+    Interceptor provideLoggingInterceptor() {
+        return new HttpLoggingInterceptor()
+                .setLevel(HttpLoggingInterceptor.Level.BODY);
+    }
+
+    @Provides
+    @Singleton
+    Interceptor provideAPIKeyInterceptor() {
+        return chain -> {
+            HttpUrl originalUrl = chain.request().url();
+            HttpUrl modifiedUrl = originalUrl.newBuilder()
+                    .addQueryParameter("appid", Constants.API_KEY)
+                    .addQueryParameter("units", Constants.UNITS)
+                    .build();
+            Request request = chain.request().newBuilder()
+                    .url(modifiedUrl)
+                    .build();
+            return chain.proceed(request);
+        };
+    }
+
+
+    @Provides
+    @Singleton
+    OkHttpClient provideOkHttpClient( Interceptor loggingInterceptor, Interceptor apiInterceptor) {
+        return new OkHttpClient.Builder()
+                .addInterceptor(loggingInterceptor)
+                .addInterceptor(apiInterceptor)
+                .connectTimeout(Constants.REQUEST_TIMEOUT, TimeUnit.SECONDS)
+                .readTimeout(Constants.REQUEST_TIMEOUT, TimeUnit.SECONDS)
+                .build();
+    }
 
     @Singleton
     @Provides
