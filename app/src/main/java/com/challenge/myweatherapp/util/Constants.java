b926e08d93f5c808e3c52ae0235c906a7eaba1bc
Joseph <user.example.com>
2019-03-02 17:19:12 +0000

    Add new constants to Constants file

--- app/src/main/java/com/challenge/myweatherapp/util/Constants.java
+++ app/src/main/java/com/challenge/myweatherapp/util/Constants.java
@@ -2,6 +2,9 @@ package com.challenge.myweatherapp.util;
 
 public class Constants {
 
-    public static final String BASE_URL = "";
-    public static final String RELATIVE_URL = "";
+    public static final String BASE_URL = "http://api.openweathermap.org/data/2.5/";
+    public static final String RELATIVE_URL = "forecast";
+    public static final String API_KEY = "1c53e16dde524f8259f97ad9ff5412a3";
+    public static final long REQUEST_TIMEOUT = 60;
+    public static final String UNITS = "metric";
 }
