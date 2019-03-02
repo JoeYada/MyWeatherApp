75cb8342ecd645ab8622300253958b37ae9f07cd
Joseph <user.example.com>
2019-03-02 17:20:34 +0000

    Fix Dagger error by adding @singleton annotation to ApplicationComponent

--- app/src/main/java/com/challenge/myweatherapp/di/components/ApplicationComponent.java
+++ app/src/main/java/com/challenge/myweatherapp/di/components/ApplicationComponent.java
@@ -4,10 +4,13 @@ import com.challenge.myweatherapp.base.BaseApplication;
 import com.challenge.myweatherapp.di.modules.ActivityBuilderModule;
 import com.challenge.myweatherapp.di.modules.ApplicationModule;
 
+import javax.inject.Singleton;
+
 import dagger.Component;
 import dagger.android.AndroidInjector;
 import dagger.android.support.AndroidSupportInjectionModule;
 
+@Singleton
 @Component(modules = {AndroidSupportInjectionModule.class, ApplicationModule.class, ActivityBuilderModule.class})
 public interface ApplicationComponent extends AndroidInjector<BaseApplication> {
     @Component.Builder
