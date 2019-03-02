bb4faa63f1537d08be39e91153e2d64d62841b9a
Joseph <user.example.com>
2019-03-02 16:08:58 +0000

    add http GET method to weather service to retrieve data

--- app/src/main/java/com/challenge/myweatherapp/service/WeatherService.java
+++ app/src/main/java/com/challenge/myweatherapp/service/WeatherService.java
@@ -1,4 +1,15 @@
 package com.challenge.myweatherapp.service;
 
+import com.challenge.myweatherapp.model.WeatherResults;
+import com.challenge.myweatherapp.util.Constants;
+
+import io.reactivex.Single;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
 public interface WeatherService {
+
+    @GET(Constants.RELATIVE_URL)
+    Single<WeatherResults> getWeatherForLocation(@Query("lat") float latitude,
+                                                 @Query("lon") float longitude);
 }
