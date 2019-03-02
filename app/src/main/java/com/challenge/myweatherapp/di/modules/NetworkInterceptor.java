ebb254204376f1e602424dff6c617203c6bde02e
Joseph <user.example.com>
2019-03-02 19:05:02 +0000

    Add qualifiers

---
+++ app/src/main/java/com/challenge/myweatherapp/di/modules/NetworkInterceptor.java
@@ -0,0 +1,11 @@
+package com.challenge.myweatherapp.di.modules;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+import javax.inject.Qualifier;
+
+@Qualifier
+@Retention(RetentionPolicy.RUNTIME)
+public @interface NetworkInterceptor {
+}
