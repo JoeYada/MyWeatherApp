e2a7f2b8feae53c568ecd05d3cef44a392c3478b
Joseph <user.example.com>
2019-03-02 19:23:11 +0000

    update base_url and relative_url

--- app/src/main/java/com/challenge/myweatherapp/util/Constants.java
+++ app/src/main/java/com/challenge/myweatherapp/util/Constants.java
@@ -2,8 +2,8 @@ package com.challenge.myweatherapp.util;
 
 public class Constants {
 
-    public static final String BASE_URL = "http://api.openweathermap.org/data/2.5/";
-    public static final String RELATIVE_URL = "forecast";
+    public static final String BASE_URL = "http://api.openweathermap.org";
+    public static final String RELATIVE_URL = "/data/2.5/forecast";
     public static final String API_KEY = "1c53e16dde524f8259f97ad9ff5412a3";
     public static final long REQUEST_TIMEOUT = 60;
     public static final String UNITS = "metric";
