28d745ff96aec4d7577e256817fe998657bff611
Joseph <user.example.com>
2019-03-03 11:18:51 +0000

    update utils and constants

--- app/src/main/java/com/challenge/myweatherapp/util/Constants.java
+++ app/src/main/java/com/challenge/myweatherapp/util/Constants.java
@@ -7,6 +7,6 @@ public class Constants {
     public static final String CURRENT_WEATHER_RELATIVE_URL = "/data/2.5/weather";
     public static final String API_KEY = "1c53e16dde524f8259f97ad9ff5412a3";
     public static final long REQUEST_TIMEOUT = 60;
-    public static final String IMAGE_URL = "https://openweathermap.org/img/w/";
+    public static final String WEATHER_IMAGE_URL = "https://openweathermap.org/img/w/";
     public static final String UNITS = "metric";
 }
--- app/src/main/java/com/challenge/myweatherapp/util/WeatherAppUtils.java
+++ app/src/main/java/com/challenge/myweatherapp/util/WeatherAppUtils.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
+import com.challenge.myweatherapp.model.WeatherResult;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -12,6 +13,8 @@ import java.util.Locale;
 public class WeatherAppUtils {
 
     public static final String DATE_PATTERN = "EE dd MMM yyyy, HH:mm";
+    public static final String DATE_PATTERN_NO_YEAR = "EE dd MMM, HH:mm";
+
 
 
     public static void loadImage(Context context, String imageUrl, ImageView imageView) {
@@ -25,5 +28,16 @@ public class WeatherAppUtils {
         Date currentDate = new Date();
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat(DATE_PATTERN, Locale.getDefault());
         return simpleDateFormat.format(currentDate);
+    }
+
+    public static String getFormattedDate(int dateTime) {
+        long timeStampMillis = dateTime * 1000L;
+        Date date = new Date(timeStampMillis);
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(DATE_PATTERN_NO_YEAR, Locale.getDefault());
+        return simpleDateFormat.format(date);
+    }
+
+    public static String getWeatherImageUrl(WeatherResult weatherResult) {
+        return Constants.WEATHER_IMAGE_URL + weatherResult.getWeather().get(0).getIcon() + ".png";
     }
 }
